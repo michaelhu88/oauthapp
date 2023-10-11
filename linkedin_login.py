067484c293036aa6e1cc0e11a175214a49959ca3
Michael Hu <michaelhu88@gmail.com>
2023-10-11 13:28:42 -0700

    Updated linkedin_login.py, ditched the oauth idea

--- linkedin_login.py
+++ linkedin_login.py
@@ -1,37 +1,28 @@
-from flask import Flask, redirect, url_for, session, request
-import uuid
-import requests
-from requests_oauthlib import OAuth2Session
 import os
+from dotenv import load_dotenv
+from linkedin_api import Linkedin
 
-app = Flask(__name__)
-app.secret_key = 'Talent_Synergy'  #a random secret key
-app.config['SESSION_TYPE'] = 'filesystem'
+load_dotenv()
 
-# Replaced with LinkedIn Credentials
-client_id = '862y79fkc4j2e8'
-client_secret = 'Evk7TliZOCIK2umC'
-authorization_base_url = 'https://www.linkedin.com/oauth/v2/authorization'
-token_url = 'https://www.linkedin.com/oauth/v2/accessToken'
-redirect_uri = 'http://127.0.0.1:5000/auth/linkedin/callback'
+email = os.getenv('EMAIL')
+password = os.getenv('PASSWORD')
 
-@app.route('/')
-def index():
-    return 'Welcome to LinkedIn OAuth'
+def get_user_info(profile_url):
+    # Extract the vanity name from the URL
+    vanity_name = profile_url.split('/')[-1]
+    
+    # Authenticate with LinkedIn
+    api = Linkedin(email, password)
+    
+    # Get the user's profile information
+    user_info = api.get_profile(vanity_name)
+    
+    # Print the user's profile information
+    print("\n")
+    for key, value in user_info.items():
+        print(f"{key}: {value}")
+        print("\n")
 
-@app.route('/auth/linkedin')
-def linkedin_auth():
-    linkedin = OAuth2Session(client_id, redirect_uri=redirect_uri)
-    authorization_url, state = linkedin.authorization_url(authorization_base_url)
-    session['oauth_state'] = state
-    return redirect(authorization_url)
-
-@app.route('/auth/linkedin/callback')
-def linkedin_callback():
-    linkedin = OAuth2Session(client_id, state=session['oauth_state'], redirect_uri=redirect_uri)
-    token = linkedin.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
-    user_info = linkedin.get('https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))')
-    return f"Hello, {user_info.json()['firstName']['localized']['en_US']} {user_info.json()['lastName']['localized']['en_US']}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Ask the end user for the profile URL
+profile_url = input("Enter the LinkedIn profile URL: ")
+get_user_info(profile_url)
